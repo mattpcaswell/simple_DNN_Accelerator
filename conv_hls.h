9873cbac1eed538ed5c1a1d729310d60beb65661
Matthew Caswell <mattpcaswell@gmail.com>
2019-04-02 13:41:41 -0400

    Update conv_hls.h

--- conv_hls.h
+++ conv_hls.h
@@ -5,10 +5,14 @@ struct point_t
 	int x, y, z;
 };
 
-typedef int data_t;
+//typedef int data_t;
+
+//typedef ap_int<8> data_t;
 
 // 16 bits wide, 8 bit integer part, convergent rounding
-//typedef ap_fixed<16,0,AP_TRN> data_t;
+typedef ap_fixed<32,32,AP_TRN,AP_SAT> activation_t;
+typedef ap_fixed<32,32,AP_TRN,AP_SAT> weight_t;
+typedef ap_fixed<32,32,AP_TRN,AP_SAT> accumulation_t;
 
 #define MAX_FILTERS_SIZE 864
 #define MAX_IN_SIZE 3072
@@ -16,15 +20,15 @@ typedef int data_t;
 #define MAX_BIAS_SIZE 32
 
 void conv(
-		data_t filters[MAX_FILTERS_SIZE],
+		weight_t filters[MAX_FILTERS_SIZE],
 		int num_filters,
 		int filter_dim_size,
-		data_t in[MAX_IN_SIZE],
+		activation_t in[MAX_IN_SIZE],
 		int in_sx,
 		int in_sy,
 		int in_sz,
-		data_t out[MAX_OUT_SIZE],
+		activation_t out[MAX_OUT_SIZE],
 		int out_sx,
 		int out_sy,
 		int stride,
-		data_t bias[MAX_BIAS_SIZE]);
+		activation_t bias[MAX_BIAS_SIZE]);
