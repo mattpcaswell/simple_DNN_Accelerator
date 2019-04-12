e0133e2c91d110c25187d85de769097d5fb45112
Matthew Caswell <mattpcaswell@gmail.com>
2019-04-12 11:59:35 -0400

    rearrange loops according to paper

--- conv_hls.cpp
+++ conv_hls.cpp
@@ -2,19 +2,13 @@
 #include <assert.h>
 using namespace std;
 
-point_t map_to_input(point_t out, int z, int stride) {
-	out.x *= stride;
-	out.y *= stride;
-	out.z = z;
-	return out;
-}
-
-int calc_filter_index(int filter_num, int x, int y, int z, int filter_size) {
+int calc_filter_index(int filter_num, int x, int y, int z, int filter_size, int filter_sz) {
+#pragma HLS INLINE
 	assert(x >= 0 && x < filter_size);
 	assert(y >= 0 && y < filter_size);
-	assert(z >= 0 && z < filter_size);
+	assert(z >= 0 && z < filter_sz);
 
-	int i = filter_num * filter_size * filter_size * filter_size; // get to the right filter
+	int i = filter_num * filter_size * filter_size * filter_sz; // get to the right filter
 	i += z * (filter_size * filter_size);
 	i += x * (filter_size);
 	i += y;
@@ -23,6 +17,7 @@ int calc_filter_index(int filter_num, int x, int y, int z, int filter_size) {
 }
 
 int calc_index(int x, int y, int z, int sx, int sy) {
+#pragma HLS INLINE
 	assert(x >= 0 && x < sx);
 	assert(y >= 0 && y < sy);
 
@@ -37,6 +32,7 @@ void conv(weight_t filters[MAX_FILTERS_SIZE], int num_filters,
 		int filter_dim_size, activation_t in[MAX_IN_SIZE], int in_sx, int in_sy,
 		int in_sz, activation_t out[MAX_OUT_SIZE], int out_sx, int out_sy,
 		int stride, activation_t bias[MAX_BIAS_SIZE]) {
+#pragma HLS ARRAY_PARTITION variable=filters cyclic factor=4 dim=1
 #pragma HLS INTERFACE bram port=filters
 #pragma HLS INTERFACE bram port=in
 #pragma HLS INTERFACE bram port=out
@@ -52,6 +48,23 @@ void conv(weight_t filters[MAX_FILTERS_SIZE], int num_filters,
 #pragma HLS INTERFACE s_axilite bundle=ctrl port=out_sy
 #pragma HLS INTERFACE s_axilite bundle=ctrl port=stride
 
+	assert(num_filters <= MAX_NUM_FILTERS);
+	assert(filter_dim_size <= MAX_FILTER_DIM);
+	assert(in_sx <= MAX_INPUT_SX);
+	assert(in_sy <= MAX_INPUT_SY);
+	assert(in_sz <= MAX_INPUT_SZ);
+	assert(out_sx <= MAX_OUTPUT_SX);
+	assert(out_sy <= MAX_OUTPUT_SY);
+
+	// Zero out the output
+	for (int x = 0; x < out_sx; x++) {
+		for (int y = 0; y < out_sy; y++) {
+			for (int z = 0; z < num_filters; z++) {
+				out[calc_index(x, y, z, out_sx, out_sy)] = bias[z];
+			}
+		}
+	}
+
 	int needed_in_sx = out_sx * stride + filter_dim_size - 1;
 	int needed_in_sy = out_sy * stride + filter_dim_size - 1;
 
@@ -60,41 +73,47 @@ void conv(weight_t filters[MAX_FILTERS_SIZE], int num_filters,
 	int pady = (needed_in_sy - in_sy) / 2;
 
 	int current_location_x, current_location_y;
-	for (int filter = 0; filter < num_filters; filter++) { 		// filter
-		for (int x = 0; x < out_sx; x++) {     					// output x
-			for (int y = 0; y < out_sy; y++) { 					// output y
-				point_t input = { x, y, 0 };
-				point_t mapped = map_to_input(input, 0, stride);
-				accumulation_t sum = 0;
-				for (int i = 0; i < MAX_FILTER_DIM; i++)     	// filter x
+	for (int i = 0; i < MAX_FILTER_DIM; i++) {    										// filter x (K)
+		if (i < filter_dim_size) {
+			for (int j = 0; j < MAX_FILTER_DIM; j++) {									// filter y (K)
+				if (j < filter_dim_size) {
+					for (int x = 0; x < MAX_OUTPUT_SX; x++) {     						// output x (R)
+						if (x < out_sx) {
+							for (int y = 0; y < MAX_OUTPUT_SY; y++) { 					// output y (C)
 #pragma HLS PIPELINE
-					if (i < filter_dim_size) {
-						for (int j = 0; j < MAX_FILTER_DIM; j++)	// filter y
+								if (y < out_sy) {
+									for (int z = 0; z < MAX_NUM_FILTERS; z++) {			// output z (M)
+#pragma HLS DEPENDENCE variable=out inter false
 #pragma HLS UNROLL
-							if (j < filter_dim_size) {
-								for (int z = 0; z < MAX_INPUT_SZ; z++) { // filter z
-#pragma HLS UNROLL
-									if (z < in_sz) {
-										weight_t f = filters[calc_filter_index(filter, i, j, z, filter_dim_size)];
-										activation_t v = 0;
-										
-										current_location_x = mapped.x + i - padx;
-										current_location_y = mapped.y + j - pady;
-										// Keep v at 0 if the location is outside of the input AKA zero padding
-										if (current_location_x >= 0
-												&& current_location_x < in_sx
-												&& current_location_y >= 0
-												&& current_location_y < in_sy)
-											v = in[calc_index(current_location_x, current_location_y, z, in_sx, in_sy)];
+										if (z < num_filters) {
+											accumulation_t sum = 0;
 
-										sum += f * v;
+											for (int k = 0; k < MAX_INPUT_SZ; k++) {	// filter z (N)
+#pragma HLS UNROLL
+												if (k < in_sz) {
+													current_location_x = (x * stride) + i - padx;
+													current_location_y = (y * stride) + j - pady;
+													// Keep v at 0 if the location is outside of the input AKA zero padding
+													if (current_location_x >= 0
+															&& current_location_x < in_sx
+															&& current_location_y >= 0
+															&& current_location_y < in_sy) {
+														weight_t f = filters[calc_filter_index(z, i, j, k, filter_dim_size, in_sz)];
+														activation_t v = in[calc_index(current_location_x, current_location_y, k, in_sx, in_sy)];
+														sum += v * f;
+													}
+												}
+											}
+
+
+											out[calc_index(x, y, z, out_sx, out_sy)] += sum / accumulation_t(256);
+										}
 									}
 								}
 							}
+						}
 					}
-				accumulation_t result = sum / accumulation_t(256); // 2^8 = 256
-				result = result + bias[filter];
-				out[calc_index(x, y, filter, out_sx, out_sy)] = result;
+				}
 			}
 		}
 	}
